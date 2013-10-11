5ad729876b5ec7e5f583e1cb4659e4cbfe4632dc
Robert Collins <rbtcollins@hp.com>
2013-10-11 22:59:22 +1300

    Setup CD overcloud public networking.
    Change-Id: I02f02bc5a99d8fc847fd47e9f16e14d2c5ec8270

--- elements/tripleo-cd/tripleo-cd.sh
+++ elements/tripleo-cd/tripleo-cd.sh
@@ -21,7 +21,7 @@ while true; do
     source /opt/stack/tripleo-incubator/scripts/setup-env /opt/stack
     source /root/stackrc
     set +u
-    devtest_overcloud.sh
+    devtest_overcloud.sh vlan25 138.35.77.4/25 eth2 138.35.77.1
     RESULT=$?
     set -u
     MSG=$(echo "************** overcloud complete status=$RESULT ************")
