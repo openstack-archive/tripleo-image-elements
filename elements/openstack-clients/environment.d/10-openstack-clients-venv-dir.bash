923c096d228cff717845e04f4c70e1fa5a5c7e61
Dan Prince <dprince@redhat.com>
2014-03-12 12:24:46 -0400

    Openstack-clients: don't hard code venv
    Updates install-openstack-clients so that it parses args rather than
    using positional parameters.
    
    Updates the various source install elements so that we use a
    new {CLIENTNAME}_VENV_DIR variable (set via an environment.d hook)
    to configure the virtualenv directory. The in element default is
    the same as before (/opt/stack/venvs/python-clientname)
    
    We also add support for controlling the virtualenv
    --system-site-packages options by passing -s to the
    install-openstack-client bin. This is externally
    controllable by setting the {CLIENTNAME}_EXTRA_INSTALL_OPTS
    variable. (defaults to "")
    
    This is cleaner in that we aren't hard coding /opt/stack/venv
    everywhere and also supports both isolated and shared
    venvs (which some people would like to use).
    
    Change-Id: Ieb90be14a5fb1a644d253777d6468870995372b0

---
+++ elements/openstack-clients/environment.d/10-openstack-clients-venv-dir.bash
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# ceilometerclient
+if [ -z "${CEILOMETERCLIENT_VENV_DIR:-}" ]; then
+    export CEILOMETERCLIENT_VENV_DIR=${OPENSTACK_VENV_DIR:-"/opt/stack/venvs/python-ceilometerclient"}
+fi
+if [ -z "${CEILOMETERCLIENT_EXTRA_INSTALL_OPTS:-}" ]; then
+    export CEILOMETERCLIENT_EXTRA_INSTALL_OPTS=${OPENSTACK_EXTRA_INSTALL_OPTS:-""}
+fi
+
+# cinderclient
+if [ -z "${CINDERCLIENT_VENV_DIR:-}" ]; then
+    export CINDERCLIENT_VENV_DIR=${OPENSTACK_VENV_DIR:-"/opt/stack/venvs/python-cinderclient"}
+fi
+if [ -z "${CINDERCLIENT_EXTRA_INSTALL_OPTS:-}" ]; then
+    export CINDERCLIENT_EXTRA_INSTALL_OPTS=${OPENSTACK_EXTRA_INSTALL_OPTS:-""}
+fi
+
+# glanceclient
+if [ -z "${GLANCECLIENT_VENV_DIR:-}" ]; then
+    export GLANCECLIENT_VENV_DIR=${OPENSTACK_VENV_DIR:-"/opt/stack/venvs/python-glanceclient"}
+fi
+if [ -z "${GLANCECLIENT_EXTRA_INSTALL_OPTS:-}" ]; then
+    export GLANCECLIENT_EXTRA_INSTALL_OPTS=${OPENSTACK_EXTRA_INSTALL_OPTS:-""}
+fi
+
+# heatclient
+if [ -z "${HEATCLIENT_VENV_DIR:-}" ]; then
+    export HEATCLIENT_VENV_DIR=${OPENSTACK_VENV_DIR:-"/opt/stack/venvs/python-heatclient"}
+fi
+if [ -z "${HEATCLIENT_EXTRA_INSTALL_OPTS:-}" ]; then
+    export HEATCLIENT_EXTRA_INSTALL_OPTS=${OPENSTACK_EXTRA_INSTALL_OPTS:-""}
+fi
+
+# ironicclient
+if [ -z "${IRONICCLIENT_VENV_DIR:-}" ]; then
+    export IRONICCLIENT_VENV_DIR=${OPENSTACK_VENV_DIR:-"/opt/stack/venvs/python-ironicclient"}
+fi
+if [ -z "${IRONICCLIENT_EXTRA_INSTALL_OPTS:-}" ]; then
+    export IRONICCLIENT_EXTRA_INSTALL_OPTS=${OPENSTACK_EXTRA_INSTALL_OPTS:-""}
+fi
+
+# keystoneclient
+if [ -z "${KEYSTONECLIENT_VENV_DIR:-}" ]; then
+    export KEYSTONECLIENT_VENV_DIR=${OPENSTACK_VENV_DIR:-"/opt/stack/venvs/python-keystoneclient"}
+fi
+if [ -z "${KEYSTONECLIENT_EXTRA_INSTALL_OPTS:-}" ]; then
+    export KEYSTONECLIENT_EXTRA_INSTALL_OPTS=${OPENSTACK_EXTRA_INSTALL_OPTS:-""}
+fi
+
+# neutronclient
+if [ -z "${NEUTRONCLIENT_VENV_DIR:-}" ]; then
+    export NEUTRONCLIENT_VENV_DIR=${OPENSTACK_VENV_DIR:-"/opt/stack/venvs/python-neutronclient"}
+fi
+if [ -z "${NEUTRONCLIENT_EXTRA_INSTALL_OPTS:-}" ]; then
+    export NEUTRONCLIENT_EXTRA_INSTALL_OPTS=${OPENSTACK_EXTRA_INSTALL_OPTS:-""}
+fi
+
+# novaclient
+if [ -z "${NOVACLIENT_VENV_DIR:-}" ]; then
+    export NOVACLIENT_VENV_DIR=${OPENSTACK_VENV_DIR:-"/opt/stack/venvs/python-novaclient"}
+fi
+if [ -z "${NOVACLIENT_EXTRA_INSTALL_OPTS:-}" ]; then
+    export NOVACLIENT_EXTRA_INSTALL_OPTS=${OPENSTACK_EXTRA_INSTALL_OPTS:-""}
+fi
+
+# swiftclient
+if [ -z "${SWIFTCLIENT_VENV_DIR:-}" ]; then
+    export SWIFTCLIENT_VENV_DIR=${OPENSTACK_VENV_DIR:-"/opt/stack/venvs/python-swiftclient"}
+fi
+if [ -z "${SWIFTCLIENT_EXTRA_INSTALL_OPTS:-}" ]; then
+    export SWIFTCLIENT_EXTRA_INSTALL_OPTS=${OPENSTACK_EXTRA_INSTALL_OPTS:-""}
+fi
