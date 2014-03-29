79f224d04aa519864a04b78133c07eee3f396704
Ben Nemec <bnemec@redhat.com>
2014-03-28 19:52:58 -0500

    Add unit tests for os-svc-daemon
    Adds a set of unit tests for os-svc-daemon to verify the output
    of running it with various options.  Includes a base test class
    with some convenience functions for testing scripts.
    
    Change-Id: Id54291686d39ab1b9f0ce4848a2fcf3c18a9ea18

---
+++ tests/base.py
@@ -0,0 +1,45 @@
+# Copyright 2014 Red Hat, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import os
+import subprocess
+import tempfile
+
+from oslotest import base
+
+
+class ScriptTestBase(base.BaseTestCase):
+    def setUp(self):
+        super(ScriptTestBase, self).setUp()
+        self.tmpdir = tempfile.mkdtemp()
+        self.env = os.environ.copy()
+        self.env['PATH'] = self.env['PATH'] + ':' + self.tmpdir
+
+    def _stub_script(self, name, contents):
+        filename = os.path.join(self.tmpdir, name)
+        with open(filename, 'w') as f:
+            f.write('#!/bin/bash\n')
+            f.write(contents)
+            f.write('\n')
+        os.chmod(filename, 0o700)
+
+    def _run_command(self, cmd):
+        try:
+            return subprocess.check_output(cmd,
+                                           stderr=subprocess.STDOUT,
+                                           env=self.env)
+        # NOTE(bnemec): If we don't handle this exception, all we get is the
+        # exit code if the command fails.
+        except subprocess.CalledProcessError as e:
+            self.fail(e.output)
