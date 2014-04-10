02194c8ffb2e16768bf00ec2e85f756e1764ec97
Dan Prince <dprince@redhat.com>
2014-04-10 10:17:38 -0400

    Horizon: dynamically set config time env vars
    Updates the Horizon element so that we inject the
    HORIZON_VENV_DIR environment variable dynamically at build time
    into the os-refresh-config/environment.d directory.
    This ensures that the correct value from the build time gets
    used during configuration and fixes an issue where the
    wrong local_settings.py file is "templated" when using
    alternate values for HORIZON_VENV_DIR.
    
    Change-Id: I00c0cb0fa03d6cce5a4625f362cc246d1461c576

--- elements/horizon/os-refresh-config/environment.d/10-horizon-venv-dir.bash
+++
@@ -1,6 +0,0 @@
-if [ -z "${HORIZON_VENV_DIR:-}" ]; then
-    export HORIZON_VENV_DIR=${OPENSTACK_VENV_DIR:-"/opt/stack/venvs/horizon"}
-fi
-if [ -z "${HORIZON_EXTRA_INSTALL_OPTS:-}" ]; then
-    export HORIZON_EXTRA_INSTALL_OPTS=${OPENSTACK_EXTRA_INSTALL_OPTS:-""}
-fi
