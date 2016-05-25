fb8dfb4aa5ae4d675175cdd0e6b22242db4a17aa
Steven Hardy <shardy@redhat.com>
2016-05-25 12:22:22 +0100

    Remove tuskar element
    We no longer support or develop tuskar, so remove this element
    
    Change-Id: I442572c7f4d1c00f5f4823ee1efc584d79ea511a

--- elements/tuskar/environment.d/10-tuskar-venv-dir.bash
+++
@@ -1,6 +0,0 @@
-if [ -z "${TUSKAR_VENV_DIR:-}" ]; then
-    export TUSKAR_VENV_DIR=${OPENSTACK_VENV_DIR:-"/opt/stack/venvs/tuskar"}
-fi
-if [ -z "${TUSKAR_EXTRA_INSTALL_OPTS:-}" ]; then
-    export TUSKAR_EXTRA_INSTALL_OPTS=${OPENSTACK_EXTRA_INSTALL_OPTS:-""}
-fi
