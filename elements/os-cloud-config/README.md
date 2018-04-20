08096ad00f98e286e7ecadba7e7e41d003217b82
Juan Antonio Osorio Robles <jaosorior@redhat.com>
2018-04-20 09:37:46 +0300

    remove os-cloud-config element
    It's not being used anymore.
    
    Change-Id: I429c0013802af3bcb80f9a623b8fce0a2e52bb64

--- elements/os-cloud-config/README.md
+++
@@ -1,4 +0,0 @@
-Install os-cloud-config
-=======================
-
-os-cloud-config contains useful utilities such as init-keystone, register-nodes
--- elements/os-cloud-config/environment.d/10-os-cloud-config-install-options.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-if [ -z "${OS_CLOUD_CONFIG_VENV_DIR:-}" ]; then
-    export OS_CLOUD_CONFIG_VENV_DIR="${OPENSTACK_VENV_DIR:-"/opt/stack/venvs/os-cloud-config"}"
-fi
